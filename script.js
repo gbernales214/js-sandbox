48b99fd89864dec31d0d46054d9fd41ab05f30bd
Gerome <gbernales214@west-mec.org>
2021-02-25 09:58:33 -0700

    Finished color selector and started dark mode toggle

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let colorSelector = document.getElementById("colorSelector");
 document.getElementById("colorSelector").onchange = function(){
     console.log(colorSelector.value);
+    document.getElementById("colorBox").style = `background-color: ${colorSelector.value}`;
 };
+
+function toggleDarkMode(){
+    
+}
